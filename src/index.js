e19d75e7d06e5be39c4e6a1e249505bcb9ea2fc0
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-15 16:03:59 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  document.addEventListener("submit", function(event){
+    const input = document.querySelector("input#new-task-description").value
+    const text = document.createTextNode(`${input}`)
+    const element = document.createElement('li')
+    element.appendChild(text)
+    document.querySelector("div#list").appendChild(element)
+    event.preventDefault();
+  })
 });
