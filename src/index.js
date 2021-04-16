e7e46b33794a067e281ff8c52c04fefcc461e52a
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-04-15 17:29:04 -0700

    submitting lab

--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ document.addEventListener("DOMContentLoaded", () => {
   document.addEventListener("submit", function(event){
     const input = document.querySelector("input#new-task-description").value
     const text = document.createTextNode(`${input}`)
+    const button = document.createElement("button")
+    button.innerHTML = "x"
     const element = document.createElement('li')
+    button.setAttribute('id', `${input}`)
     element.appendChild(text)
+    element.appendChild(button)
+    button.addEventListener("click", function(event){
+      thisButton = event.target.id
+      buttonClick(thisButton)
+    })
     document.querySelector("div#list").appendChild(element)
     event.preventDefault();
   })
+
+  function buttonClick(button){
+    document.getElementById(button).parentElement.remove()
+  }
 });
